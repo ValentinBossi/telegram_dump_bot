78e0b4c20b94b43622fae7ae4c02a73fd366accb
Valentin <bossi.valentin@gmail.com>
2019-09-25 14:23:37 +0200

    simplified echo, event 'message' is always called but bot answers only if he is mentioned with '/stm' or he is in a "private" chat

--- index.ts
+++ index.ts
@@ -13,30 +13,19 @@ app.listen(port, function () {
 });
 
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = '954158159:AAFRwclM4s-ERRLbUicebYpGR_0Oz7cuLtA';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
+const botTriggerRegExp = RegExp('^\\/stm (.*)');
 
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, '😘 ' + msg.from.first_name);
+    if (botTrigger(msg)) {
+        // send a message to the chat acknowledging receipt of their message
+        bot.sendMessage(chatId, '😘 ' + msg.from.first_name + ' ::::msg: ' + msg.text);
+    }
 });
+
+const botTrigger = (msg: TelegramBot.Message) => botTriggerRegExp.test(msg.text) || msg.chat.type === 'private';
